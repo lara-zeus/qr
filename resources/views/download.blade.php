01566d2ec4fdd3d88e307cc5fa2dacdc8add8797
Ash Monsh <wh7r.com@gmail.com>
2024-01-02 23:23:00 +0300

    ✨ make it easier to render QR anywhere in your app

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -1,31 +1,24 @@
-@php use Filament\Support\Facades\FilamentAsset; @endphp
-<div>
-    <div
-        class="flex items-center justify-center"
-        x-ignore
-        ax-load-src="{{ FilamentAsset::getAlpineComponentSrc('qr','lara-zeus/qr') }}"
-        ax-load
-        x-data="qrPlugin({
-            state: '{{ $parentName }}',
-        })"
-    >
+<div
+    class="flex flex-col gap-2 items-center justify-center"
+    x-ignore
+    ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('qr','lara-zeus/qr') }}"
+    ax-load
+    x-data="qrPlugin({
+        state: '{{ $statePath }}',
+    })"
+>
+    <div class="{{ $statePath }} flex flex-col justify-center items-center">
+        {{ \LaraZeus\Qr\Facades\Qr::output($data,$options) }}
     </div>
 
-    <div class="{{ $parentName }} text-center flex flex-col justify-center items-center">
-
-        {!! \LaraZeus\Qr\Facades\Qr::qrRender($$optionsName, $url) !!}
-
+    @if($downloadable)
         <x-filament::button
             color="info"
             size="sm"
             icon="heroicon-o-arrow-down-tray"
-            class="my-2"
-            x-data=""
-            @click="download('{{ $parentName }}');"
-            {{--@click="$dispatch('async-alpine:load', { id: 'qrPlugin' })"--}}
+            @click="download('{{ $statePath }}');"
         >
-            Download
+            {{ __('Download') }}
         </x-filament::button>
-
-    </div>
+    @endif
 </div>
